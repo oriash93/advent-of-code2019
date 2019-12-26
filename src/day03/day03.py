7da70da2456f2ae2068044c964121fcd3528b7bf
oriash93 <oriash93@gmail.com>
2019-12-26 15:38:12 +0200

    day03 - part2

--- src/day03/day03.py
+++ src/day03/day03.py
@@ -1,25 +1,34 @@
-def traverse(commands):
+def traverse(commands, positions_dict):
     positions = list()
     x, y = 0, 0
+    steps = 0
     for command in commands:
         direction = command[0]
         delta = int(command[1:])
         if direction == 'U':
             for i in range(1, delta + 1):
+                steps += 1
                 y += 1
                 positions.append((x, y))
+                positions_dict[(x, y)] = steps
         elif direction == 'D':
             for i in range(1, delta + 1):
+                steps += 1
                 y -= 1
                 positions.append((x, y))
+                positions_dict[(x, y)] = steps
         elif direction == 'L':
             for i in range(1, delta + 1):
+                steps += 1
                 x -= 1
                 positions.append((x, y))
+                positions_dict[(x, y)] = steps
         elif direction == 'R':
             for i in range(1, delta + 1):
+                steps += 1
                 x += 1
                 positions.append((x, y))
+                positions_dict[(x, y)] = steps
         else:
             raise Exception('invalid!')
             return
@@ -27,7 +36,7 @@ def traverse(commands):
 
 
 def intersect(col1, col2):
-    return [item for item in col1 if item in col2]
+    return set(col1).intersection(col2)
 
 
 def distance_from_origin(x, y):
@@ -39,13 +48,18 @@ def main():
     with open(file_path, 'r') as input_file:
         line1, line2 = input_file.readlines()
 
+    positions_dict1 = dict()
+    positions_dict2 = dict()
     commands1 = [command.strip() for command in line1.split(',')]
-    positions1 = traverse(commands1)
+    positions1 = traverse(commands1, positions_dict1)
     commands2 = [command.strip() for command in line2.split(',')]
-    positions2 = traverse(commands2)
+    positions2 = traverse(commands2, positions_dict2)
     intersections = intersect(positions1, positions2)
     part1 = min([distance_from_origin(x, y) for x, y in intersections])
     print('part1 =', part1)
+    part2 = min([positions_dict1[intersection] + positions_dict2[intersection]
+                 for intersection in intersections])
+    print('part2 =', part2)
 
 
 if __name__ == '__main__':
