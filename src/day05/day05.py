482f6324557d27a738689545b5b8e2725cc601e1
oriash93 <oriash93@gmail.com>
2020-01-11 22:45:38 +0200

    day05 - part2

--- src/day05/day05.py
+++ src/day05/day05.py
@@ -33,6 +33,48 @@ def handle_output(param1, commands, mode):
     outputs.append(param1)
 
 
+def handle_jump_if_true(param1, param2, commands, mode1, mode2):
+    if mode1 != 1:
+        param1 = commands[param1]
+    if mode2 != 1:
+        param2 = commands[param2]
+    if param1 != 0:
+        result = param2
+        return result
+
+
+def handle_jump_if_false(param1, param2, commands, mode1, mode2):
+    if mode1 != 1:
+        param1 = commands[param1]
+    if mode2 != 1:
+        param2 = commands[param2]
+    if param1 == 0:
+        result = param2
+        return result
+
+
+def handle_less_than(param1, param2, param3, commands, mode1, mode2):
+    if mode1 != 1:
+        param1 = commands[param1]
+    if mode2 != 1:
+        param2 = commands[param2]
+    if param1 < param2:
+        commands[param3] = 1
+    else:
+        commands[param3] = 0
+
+
+def handle_equals(param1, param2, param3, commands, mode1, mode2):
+    if mode1 != 1:
+        param1 = commands[param1]
+    if mode2 != 1:
+        param2 = commands[param2]
+    if param1 == param2:
+        commands[param3] = 1
+    else:
+        commands[param3] = 0
+
+
 def handle_instruction(opcode, commands, index, mode1=0, mode2=0, mode3=1):
     if opcode == 1 or opcode == 2:
         param1 = commands[index + 1]
@@ -53,6 +95,34 @@ def handle_instruction(opcode, commands, index, mode1=0, mode2=0, mode3=1):
         param1 = commands[index + 1]
         handle_output(param1, commands, mode1)
         index = index + 2
+    elif opcode == 5:
+        param1 = commands[index + 1]
+        param2 = commands[index + 2]
+        result = handle_jump_if_true(param1, param2, commands, mode1, mode2)
+        if result is not None:
+            index = result
+        else:
+            index = index + 3
+    elif opcode == 6:
+        param1 = commands[index + 1]
+        param2 = commands[index + 2]
+        result = handle_jump_if_false(param1, param2, commands, mode1, mode2)
+        if result is not None:
+            index = result
+        else:
+            index = index + 3
+    elif opcode == 7:
+        param1 = commands[index + 1]
+        param2 = commands[index + 2]
+        param3 = commands[index + 3]
+        handle_less_than(param1, param2, param3, commands, mode1, mode2)
+        index = index + 4
+    elif opcode == 8:
+        param1 = commands[index + 1]
+        param2 = commands[index + 2]
+        param3 = commands[index + 3]
+        handle_equals(param1, param2, param3, commands, mode1, mode2)
+        index = index + 4
     elif opcode == 99:  # exit
         return len(commands)
     else:
